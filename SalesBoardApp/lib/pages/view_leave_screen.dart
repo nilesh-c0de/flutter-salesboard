f9fd28efbfca12f14475d56aefbcde425ec80d64
Pioneer <omkar.bongale1393@gmail.com>
2024-10-22 12:01:18 +0530

    added view leave

---
+++ SalesBoardApp/lib/pages/view_leave_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ViewLeaveScreen extends StatefulWidget {
+  const ViewLeaveScreen({super.key});
+
+  @override
+  State<ViewLeaveScreen> createState() => _ViewLeaveScreenState();
+}
+
+class _ViewLeaveScreenState extends State<ViewLeaveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Scaffold(
+        body: Center(
+          child: Text("Welcome to Leaves"),
+        ),
+      ),
+    );
+  }
+}
